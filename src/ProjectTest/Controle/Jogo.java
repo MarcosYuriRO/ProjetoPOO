05a14a9d89c6d8d7ed631a59e2fd9f85668e7bfd
Yuri <marcos.yuriro@gmail.com>
2025-09-29 18:42:14 -0300

    feat(Controle): merge dispute resolution

--- src/ProjectTest/Controle/Jogo.java
+++ src/ProjectTest/Controle/Jogo.java
@@ -87,17 +87,14 @@ public class Jogo {
         }
 
 
-        //Insere o imigrante instanciado. caso a validade não esteja vencida e o nome e a idade coincidam com os ditos, retorna true. Se não, false
-      public boolean verificarLegalidade(Imigrante dadosImigrante, Identidade dadosIdentidade) {
+    //Insere o imigrante instanciado. caso a validade não esteja vencida e o nome e a idade coincidam com os ditos, retorna true. Se não, false
+      public boolean verificarLegalidade(Imigrante dadosImigrante) {
             if(){
                 return true;
             } else {
                 return false;
             }
         }
-    //Diminui o ano atual, pelo ano de nascimento presente na identidade, resultando na idade
-    public int calcularIdade(){
-        return LocalDate.now().getYear() - getDataNascimento().getYear();
-    }*/
+
 
 }
